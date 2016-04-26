77954b8c667b6d5b0067babdeb3500227a8be1cc
Mateusz <mateuszgabrynowicz@gmail.com>
2016-04-26 23:06:52 +0200

    adding authentication in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  resources :users
+  resources :sessions
+  get 'login' => 'sessions#new'
   resources :shopping_lists do
     resources :shopping_items do
       member do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420182827) do
+ActiveRecord::Schema.define(version: 20160426201457) do
 
   create_table "shopping_items", force: :cascade do |t|
     t.string   "content"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20160420182827) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
