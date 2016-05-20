5d6445ac45ab318015f15d7e021d054522f7b6db
Mateusz <mateuszgabrynowicz@gmail.com>
2016-05-20 22:06:49 +0200

    session changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,15 @@ def create
 	@user = User.find_by_email(params[:session][:email])
 	if @user && @user.authenticate(params[:session][:password])
 		session[:user_id] = @user.id
-		redirect_to '/'
+		redirect_to shopping_lists_path
 	else
-		redirect_to 'login'
+		redirect_to root_path
 	end
 end
 
 def destroy
 	session[:user_id] = nil
-	redirect_to '/'
+	redirect_to root_path
 end
 
 end
